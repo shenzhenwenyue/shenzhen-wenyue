b230d66ea4ab0715b823adaeb9c8c7bdf249ceda
Christiana <tu@email.com>
2026-05-06 21:04:33 -0500

    Fix: include shipping_cost in reports revenue and profit calculation

--- components/AdminReports.js
+++ components/AdminReports.js
@@ -118,10 +118,11 @@ export default function AdminReports({ orders, adminPassword }) {
         }
       })
     })
-    const totalRevenue = totalItemsRevenue
+    const totalShippingRevenue = revenueOrders.reduce((sum, o) => sum + (o.shipping_cost || 0), 0)
+    const totalRevenue = totalItemsRevenue + totalShippingRevenue
     const avgTicket = paidOrders > 0 ? totalRevenue / paidOrders : 0
     const totalCostoEnvioReal = revenueOrders.reduce((sum, o) => sum + (o.costo_envio_real || 0), 0)
-    const ganancia = totalItemsRevenue - totalCosto - totalCostoEnvioReal
+    const ganancia = totalRevenue - totalCosto - totalCostoEnvioReal
     const margen = totalItemsRevenue > 0 ? (ganancia / totalItemsRevenue) * 100 : null
 
     return { totalRevenue, totalOrders, paidOrders, avgTicket, totalUnits, pendingRevenue, ganancia, margen, tieneCostos: itemsConCosto > 0 }
